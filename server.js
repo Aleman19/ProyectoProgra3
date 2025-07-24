ddead607324b6e253fb0b220cf0c0bf76707e4b5
DanA151-coder <danaguilar946@gmail.com>
2025-07-23 18:38:17 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+require('dotenv').config();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+// Configuración del motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));  // ✅ <- Esta línea era lo que faltaba
+
+const citasRoutes = require('./routes/citas');
+app.use('/', citasRoutes);
+
+// Configuración del socket
+io.on('connection', (socket) => {
+    console.log('Cliente conectado');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+// Iniciar el servidor
+http.listen(process.env.PORT || 3000, () => {
+    console.log(`Servidor en http://localhost:${process.env.PORT || 3000}`);
+});
