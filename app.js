eed4bc76d61ef3c9c81fd2185564e3168a9dd667
DanA151-coder <danaguilar946@gmail.com>
2025-08-02 05:43:55 +0000

    Fix server instance reference in app.js to use 'app' instead of 'server'

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use('/', require('./routes/indexRoutes'));
 app.use('/citas', require('./routes/citaRoutes'));
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
