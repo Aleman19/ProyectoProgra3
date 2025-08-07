e6402fb66404d88a602148754c9c65aaaabf1945
abarrantes14 <arielbarrantes14@gmail.com>
2025-08-07 02:25:20 +0000

    Remove commented database import from app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-const db = require('./database/db'); // Asegurate de que el path es correcto
 
 
 // Middleware y archivos estaticos
