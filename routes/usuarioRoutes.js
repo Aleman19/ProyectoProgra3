778f32e4a0921a0c35ed6b033326ff2e5cb72093
DanA151-coder <danaguilar946@gmail.com>
2025-08-09 02:18:44 +0000

    Update login route in usuarioRoutes.js and modify index.ejs to reflect new path

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 
+// Mostrar página de login (GET)
+router.get('/login', (req, res) => {
+  res.render('login'); // Asegúrate que login.ejs exista en views/
+});
+
+// Procesar login (POST)
 router.post('/login', usuarioController.iniciarSesion);
 
 module.exports = router;
